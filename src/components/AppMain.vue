8bacc0d42355d14ff51735f511bd0208c2ee57c9
vgianluca96 <gianluca.vallese@hotmail.com>
2023-10-03 16:55:39 +0200

    main update
    creata prima sezione

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,7 +8,22 @@ export default {
 
 <template>
     <main>
-        <h1>Main</h1>
+
+        <div class="row row-cols-4 gx-1 py-1">
+            <div class="col">
+                <img src="../assets/img/h3-img-1.jpg" alt="" class="w-100">
+            </div>
+            <div class="col">
+                <img src="../assets/img/h3-img-2.jpg" alt="" class="w-100">
+            </div>
+            <div class="col">
+                <img src="../assets/img/h3-img-3.jpg" alt="" class="w-100">
+            </div>
+            <div class="col">
+                <img src="../assets/img/h3-img-4.jpg" alt="" class="w-100">
+            </div>
+
+        </div>
     </main>
 </template>
 
