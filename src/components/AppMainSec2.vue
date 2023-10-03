51f5b359c39e449b3945c22f562694c40aaed6ed
vgianluca96 <gianluca.vallese@hotmail.com>
2023-10-03 22:19:31 +0200

    main update
    aggiunti dettagli alla sezione testimonials

--- src/components/AppMainSec2.vue
+++ src/components/AppMainSec2.vue
@@ -48,15 +48,20 @@ export default {
 
             <div class="d-flex flex-column justify-content-center align-items-center w-50 text-uppercase text-center"
                 v-for="(item, index) in this.testimonials" :class="{ 'd-none': index != this.active }">
-                <h4>
+                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-quote"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z" />
+                </svg>
+                <h5>
                     {{ item.feedback }}
-                </h4>
+                </h5>
                 <div>
                     {{ item.authorAndDate }}
                 </div>
                 <div class="d-flex">
                     <div v-for="(n, dotIndex) in this.testimonials.length">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dot"
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-dot"
                             viewBox="0 0 16 16">
                             <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"
                                 :class="{ 'dot-red': dotIndex == this.active }" />
