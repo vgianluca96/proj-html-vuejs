48d9ed8580b61214698d32389d94dae1e98f8a31
vgianluca96 <gianluca.vallese@hotmail.com>
2023-10-03 10:39:42 +0200

    initial commit
    inzializzato progetto Vite, installati sass e bootstrap

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<template>
+  <h1 class="text-center pt-5">Hello</h1>
+</template>
+
+<style lang="scss"></style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import './assets/sass/app.scss'
+
+createApp(App).mount('#app')
