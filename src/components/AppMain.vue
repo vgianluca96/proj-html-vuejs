424e333da082fefc260a27dbe60ac419714abd75
vgianluca96 <gianluca.vallese@hotmail.com>
2023-10-03 17:59:17 +0200

    main update
    create bozze seconda e terza section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,22 +8,60 @@ export default {
 
 <template>
     <main>
+        <section>
+            <div class="row row-cols-4 m-0">
+                <div class="col p-1 ps-0">
+                    <img src="../assets/img/h3-img-1.jpg" alt="" class="w-100">
+                </div>
+                <div class="col p-1">
+                    <img src="../assets/img/h3-img-2.jpg" alt="" class="w-100">
+                </div>
+                <div class="col p-1">
+                    <img src="../assets/img/h3-img-3.jpg" alt="" class="w-100">
+                </div>
+                <div class="col p-1 pe-0">
+                    <img src="../assets/img/h3-img-4.jpg" alt="" class="w-100">
+                </div>
+            </div>
+        </section>
 
-        <div class="row row-cols-4 gx-1 py-1">
-            <div class="col">
-                <img src="../assets/img/h3-img-1.jpg" alt="" class="w-100">
-            </div>
-            <div class="col">
-                <img src="../assets/img/h3-img-2.jpg" alt="" class="w-100">
-            </div>
-            <div class="col">
-                <img src="../assets/img/h3-img-3.jpg" alt="" class="w-100">
-            </div>
-            <div class="col">
-                <img src="../assets/img/h3-img-4.jpg" alt="" class="w-100">
-            </div>
+        <section class="testimonials d-flex align-items-center">
+            <div class="d-flex justify-content-between align-items-center w-100">
 
-        </div>
+                <div class="prev-btn">
+                    <button type="button">
+                        Prev
+                    </button>
+                </div>
+
+                <div class="d-flex flex-column justify-content-center align-items-center w-50 text-uppercase text-center">
+                    <h4>
+                        "forget the trendy pizza shops, this hidden spot makes the best new york-style pizza slice in
+                        naples"
+                    </h4>
+                    <div>
+                        Washington post 2018
+                    </div>
+                </div>
+
+                <div class="next-btn">
+                    <button type="button">
+                        Next
+                    </button>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="row row-cols-2 m-0">
+                <div class="col p-1 ps-0">
+                    <img src="../assets/img/h1-img-4.jpg" alt="" class="w-100">
+                </div>
+                <div class="col p-1 pe-0">
+                    <div>ciao</div>
+                </div>
+            </div>
+        </section>
     </main>
 </template>
 
