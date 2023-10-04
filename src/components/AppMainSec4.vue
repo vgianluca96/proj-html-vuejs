a6d61649d58c346cb29a0185b7acaef41e7d5970
vgianluca96 <gianluca.vallese@hotmail.com>
2023-10-04 12:44:10 +0200

    main update
    terminata sezione 4

--- src/components/AppMainSec4.vue
+++ src/components/AppMainSec4.vue
@@ -4,9 +4,33 @@ export default {
     name: 'AppMainSec4',
     data() {
         return {
-            testimonials: [
+            blocks: [
                 {
-                    class: '',
+                    class: 'divx1 divy1',
+                },
+                {
+                    class: 'divx2 divy1',
+                },
+                {
+                    class: 'divx3 divy1',
+                },
+                {
+                    class: 'divx1 divy2',
+                },
+                {
+                    class: 'divx2 divy2',
+                },
+                {
+                    class: 'divx3 divy2',
+                },
+                {
+                    class: 'divx1 divy3',
+                },
+                {
+                    class: 'divx2 divy3',
+                },
+                {
+                    class: 'divx3 divy3',
                 },
             ]
         }
@@ -16,15 +40,15 @@ export default {
 </script>
 
 <template>
-    <section class="build-pizza">
-        <div>
-            <h2 class="text-uppercase">
-                go ahead and <br>
-                build your <br>
-                own pizza <br>
-                we won't <br>
-                judge!
-            </h2>
+    <section class="build-pizza d-flex flex-wrap position-relative">
+        <h2 class="text-uppercase">
+            go ahead and <br>
+            build your <br>
+            own pizza <br>
+            we won't <br>
+            judge!
+        </h2>
+        <div v-for="(item, index) in this.blocks" :class="item.class">
         </div>
     </section>
 </template>
